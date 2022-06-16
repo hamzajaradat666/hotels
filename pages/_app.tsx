d6634263cd05b74943b35967060be1ce4e5f5542
Hamza Jaradat <hjaradat@Madfooat.com>
2022-06-16 14:48:27 +0300

    Context Api  Removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import AppHeader from '../components/AppHeader'
-import { ThemeProvider } from '../contexts/ThemeContext'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <ThemeProvider>
         <div className='p-1 sm:p-12 fadeIn'>
         <AppHeader>
           <Component {...pageProps} />
         </AppHeader>
         </div>
-      </ThemeProvider>
   )
 
 
